bf2f00c8156f63064ebdb81c2ced10f8e3f982f1
Alexander Latyshev <aladdelinxr@gmail.com>
2022-11-17 01:56:12 +0300

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { history, store } from "./redux/store";
+import { history, persistor, store } from "./redux/store";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
 import { ReduxRouter } from "@lagunovsky/redux-react-router";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
     <ApolloProvider client={client}>
-      <ReduxRouter history={history} store={store}>
-        <App />
-      </ReduxRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <ReduxRouter history={history} store={store}>
+          <App />
+        </ReduxRouter>
+      </PersistGate>
     </ApolloProvider>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,37 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import {
   createRouterMiddleware,
   createRouterReducer,
 } from "@lagunovsky/redux-react-router";
+import storage from "redux-persist/lib/storage";
 import { createBrowserHistory } from "history";
 import { storeSlice } from "./reducers/storeSlice";
 import { dataSlice } from "./reducers/dataSlice";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedStore = persistReducer(persistConfig, storeSlice.reducer);
+
 export const history = createBrowserHistory();
+
 export const routerMiddleware = createRouterMiddleware(history);
 
 export const storeReducer = combineReducers({
-  store: storeSlice.reducer,
+  store: persistedStore,
   data: dataSlice.reducer,
   router: createRouterReducer(history),
 });
@@ -21,7 +41,12 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       immutableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }).prepend(routerMiddleware),
 });
+
+export let persistor = persistStore(store);
 
 export default store;
